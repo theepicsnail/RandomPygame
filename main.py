856a674d532ce4797edce451ae1f9e77c4734757
Steven <theepicsnail@gmail.com>
2011-08-22 23:06:04 -0400

    Added a debug display (F12)

--- main.py
+++ main.py
@@ -25,24 +25,32 @@ class Character(pygame.sprite.Sprite):
 		
 		
 	
-def draw_background(screen, tile_img):
+def draw_background(screen, tile_img, offset=(0,0)):
     img_rect = tile_img.get_rect()
+    xoff = offset[0]%img_rect.width
+    yoff = offset[1]%img_rect.height
     
-    nrows = int(screen.get_height() / img_rect.height) + 1
-    ncols = int(screen.get_width() / img_rect.width) + 1
+    nrows = int(screen.get_height() / img_rect.height) + 2
+    ncols = int(screen.get_width() / img_rect.width) + 2
     
     for y in range(nrows):
         for x in range(ncols):
-            img_rect.topleft = (x * img_rect.width, 
-                                y * img_rect.height)
+            img_rect.topleft = ((x-1) * img_rect.width+xoff, 
+                                (y-1) * img_rect.height+yoff)
             screen.blit(tile_img, img_rect)
 	
-		
+def gameprint(text,xx,yy,color):
+   font = pygame.font.SysFont("Courier New",18)
+   ren = font.render(text,1,color)
+   screen.blit(ren, (xx,yy))
+
+DEBUG=False 
 pygame.init()
 clock = pygame.time.Clock()
 
 pygame.mixer.music.load(Utils.AudioPath("bg.mp3"))
-pygame.mixer.music.play(-1,0);
+#pygame.mixer.music.play(-1,-10);#count,position(sec)
+
 screen=pygame.display.set_mode(Configuration.ScreenSize)
 grassImage = pygame.image.load(Utils.ImagePath("grass.jpg"))
 
@@ -57,8 +65,8 @@ anim.stop()
 PlayerLoc = [0,0]
 PlayerVel = [0,0]
 def handleKey(event,pressed):
-    global anim,PlayerVel
-    try:
+    global anim,PlayerVel,DEBUG
+    if event.key in Configuration.Keys:
         newDir = Configuration.Keys.index(event.key)
         scale = 1
         if not pressed:
@@ -79,16 +87,20 @@ def handleKey(event,pressed):
             anim=ss[2]
         else:
             anim=ss[3]
-        anim.iter()
-            
-    except:
-        print event,pressed
+        anim.iter() 
+    elif event.key==293:#F12
+        DEBUG=pressed
+    else:
+        print event
+    #f event.key
         
 def updatePlayer(dt):
     global PlayerLoc,PlayerVel
     PlayerLoc[0]+=PlayerVel[0]*dt/10.0
     PlayerLoc[1]+=PlayerVel[1]*dt/10.0
-    
+def displayDebug():
+    gameprint("Loc:%r"%PlayerLoc,0,0,(0,0,0))
+    gameprint("Vel:%r"%PlayerVel,0,20,(0,0,0))
 while True:
     time_passed = clock.tick(50)
     for event in pygame.event.get():
@@ -100,8 +112,11 @@ while True:
             handleKey(event,False)
             
     updatePlayer(time_passed) 
-    draw_background(screen,grassImage)
+    draw_background(screen,grassImage,map(lambda x:x/-2,PlayerLoc))
     screen.blit(anim.next(),PlayerLoc)
+    
+    if DEBUG:
+        displayDebug();
     pygame.display.flip()
     
     
